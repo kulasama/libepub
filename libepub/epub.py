40e87c84fa5c71d6532a3d0c6f95ac26f3715f36
kula <kulasama@gmail.com>
2011-03-14 10:06:10 +0800

    add chapters parse

--- libepub/epub.py
+++ libepub/epub.py
@@ -1,20 +1,34 @@
 #coding:utf8
 import unittest     
 import zipfile
-from BeautifulSoup import BeautifulStoneSoup
+from BeautifulSoup import BeautifulStoneSoup,BeautifulSoup
 
 class EPub(object) :
     
     def __init__(self,path):
-        o = zipfile.ZipFile(path, mode='r') 
-        buf = o.read('toc.ncx')      
+        self.o = zipfile.ZipFile(path, mode='r') 
+        buf = self.o.read('toc.ncx')      
         self.soap = BeautifulStoneSoup(buf, fromEncoding='utf8')  
         
     def title(self):  
         return unicode(self.soap.ncx.doctitle.text.string)     
         
     def author(self):
-        return unicode(self.soap.ncx.docauthor.text.string)
+        return unicode(self.soap.ncx.docauthor.text.string)  
+        
+    def chapters(self):
+        chapters = self.soap.findAll('navpoint',{'class':'chapter'})    
+        return chapters
+        
+    def get_chapter_content(self,chapter_id):
+        fname = '%s.html' % chapter_id
+        buf = self.o.read(fname) 
+        buf_soap = BeautifulSoup(buf,fromEncoding='utf8') 
+        body = buf_soap.html.body.decodeContents('utf8')
+        return body
+            
+    
+        
     
         
 
@@ -30,8 +44,11 @@ class EPubTestCase(unittest.TestCase):
         title = epub.title()      
         self.assertEqual(title,u'犹太商人羊皮卷')  
         author = epub.author()
-        self.assertEqual(author,u'弥赛亚')
-        
+        self.assertEqual(author,u'弥赛亚')   
+        chapters = epub.chapters() 
+        chapter = chapters[0]  
+        content = epub.get_chapter_content(chapter['id'])  
+
         
         
 
